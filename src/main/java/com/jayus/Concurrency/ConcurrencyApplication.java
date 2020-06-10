6575b4f8c63af3cb66fd4af06b9ff9bdfd0f68dd
ButtZon <46368696+ButtZon@users.noreply.github.com>
2020-06-10 15:51:53 +0800

    'init'

---
+++ src/main/java/com/jayus/Concurrency/ConcurrencyApplication.java
@@ -0,0 +1,18 @@
+package com.jayus.Concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		try{
+			SpringApplication.run(ConcurrencyApplication.class, args);
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
